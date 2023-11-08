84c89d684913357684592d72d768c8749b54094b
Just Coz <17277922+JustCozzie@users.noreply.github.com>
2023-11-08 18:02:45 +1100

    Fancy Icons

--- CoffeeHandler.gd
+++ CoffeeHandler.gd
@@ -1,16 +1,17 @@
 extends Control
-@onready var cup = $Background/HBoxContainer/VBoxContainer/HBoxContainer/Cup
-@onready var drink_name = $Background/HBoxContainer/VBoxContainer/DrinkName
+@onready var cup = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Cup
+@onready var drink_name = $Background/MarginContainer/HBoxContainer/VBoxContainer/DrinkName
 
-@onready var espresso = $Background/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Espresso
-@onready var milk = $Background/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Milk
-@onready var froth = $Background/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Froth
-@onready var chocolate = $Background/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Chocolate
+@onready var espresso = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Espresso
+@onready var milk = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Milk
+@onready var froth = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Froth
+@onready var chocolate = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Cup/ClipMask/Chocolate
 
-@onready var espresso_slide = $Background/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/EspressoSlide
-@onready var milk_slide = $Background/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/MilkSlide
-@onready var froth_slide = $Background/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/FrothSlide
-@onready var chocolate_slide = $Background/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/ChocolateSlide
+@onready var espresso_slide = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/EspressoSlide
+@onready var milk_slide = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/MilkSlide
+@onready var froth_slide = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/FrothSlide
+@onready var chocolate_slide = $Background/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/HBoxContainer/ChocolateSlide
+
 
 @onready var slideList = [espresso_slide, milk_slide, froth_slide, chocolate_slide]
 
@@ -50,13 +51,17 @@ func _process(delta):
 
 
 func _on_espresso_slide_value_changed(value):
+	espresso.set_custom_minimum_size(Vector2(0, (value / 100 * (0.95*cupSize.y))))
 	calculateCupLevel()
 
 func _on_milk_slide_value_changed(value):
+	milk.set_custom_minimum_size(Vector2(0, (value / 100 * (0.95*cupSize.y))))
 	calculateCupLevel()
 
 func _on_froth_slide_value_changed(value):
+	froth.set_custom_minimum_size(Vector2(0, (value / 100 * (0.95*cupSize.y))))
 	calculateCupLevel()
 	
 func _on_chocolate_slide_value_changed(value):
+	chocolate.set_custom_minimum_size(Vector2(0, (value / 100 * (0.95*cupSize.y))))
 	calculateCupLevel()
